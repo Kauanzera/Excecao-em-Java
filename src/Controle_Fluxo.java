e92fe02dc683301dd7666d8e15ace2e0091feece
Kauan Oliveira <kauan.nana9@gmail.com>
2024-08-09 14:06:30 -0300

    Primeira versão

---
+++ src/Controle_Fluxo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Controle_Fluxo {
+	public static void main(String[] args) {
+	
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		sc.close();
+	}
+}
